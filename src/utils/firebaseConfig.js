b992be4e6805f88da349911234864d26f7b41c68
Manuel Valera <manuelvalera@hotmail.com>
2023-10-16 20:07:32 +0200

    Actualizar firebaseConfig.js

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdkJ2RhcdUgPeQDsUzb_8eirpX9EzRXQQ",
+  apiKey: "",
   authDomain: "basketmania-e327b.firebaseapp.com",
   databaseURL: "https://basketmania-e327b-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "basketmania-e327b",
